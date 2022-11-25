113f92cff2a28ebd4b0822bd01a54afd1ca64569
Jason Kadman <jkadman231@gmail.com>
2022-11-25 00:32:35 +0000

    add code to see database

--- express_server.js
+++ express_server.js
@@ -7,9 +7,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello!');
+// });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
