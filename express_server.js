0ce0d197d5cabcbf80a0dd5f3fa0c3b6777bd300
Jason Kadman <jkadman231@gmail.com>
2022-11-25 04:52:08 +0000

    add dependencies for express, create link to views file

--- express_server.js
+++ express_server.js
@@ -10,16 +10,22 @@ const urlDatabase = {
 };
 
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 // app.get('/', (req, res) => {
-//   res.send('Hello!');
+//   const templateVars = { greeting: 'Hello World!'};
+//   res.render('urls_index', templateVars);
 // });
 
 
